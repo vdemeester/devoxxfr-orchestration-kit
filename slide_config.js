73350e138a49f31fd6749a6406c9c01d0528e280
Vincent Demeester <vincent@sbr.pm>
2017-02-07 22:18:53 +0100

    Initial content
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Orchestration kit', 
+     subtitle: 'with docker swarm mode 🐳', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/favicon.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'vdemeester', 
+     company: 'Docker Inc.', 
+     gplus: '', 
+     twitter: 'vdemeest', 
+     www: 'http://vincent.demeester.fr/', 
+     github: 'http://github.com/vdemeester', 
+   }]
+};
